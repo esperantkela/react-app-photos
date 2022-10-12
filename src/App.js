19ee01624eaa4545b876371ef72eccaebce79c26
esperantkela <esperant.kela@gmail.com>
2022-10-12 15:40:11 +0100

    navigation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,32 @@ import './App.css';
 import Welcome from './components/Welcome';
 import Car from './components/Car';
 import Login from './components/Login';
+import { BrowserRouter, Routes , Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Login/>
+    <BrowserRouter>
+
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+          <Routes>
+            <Route path='home' index element={<Welcome />} />
+            <Route path="/login" index element={<Login />} />
+          </Routes>
+          
+      </div>
+
+    </BrowserRouter>
     </>
   );
 }
