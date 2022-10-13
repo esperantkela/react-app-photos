93ffcc3861e0abf6d844b70d72df5c104baff8a5
esperantkela <esperant.kela@gmail.com>
2022-10-13 14:25:24 +0100

    handle onclick evengt

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,29 +11,61 @@ class Register extends React.Component{
         }
     }
 
+    handleNameChange = (e) =>{
+        this.setState({
+            name: e.target.value
+        }, ()=>{
+            console.log(this.state)
+        })
+    }
+
+    handleEmailChange = (e) =>{
+        this.setState({
+            email: e.target.value
+        }, ()=>{
+            console.log(this.state)
+        })
+    }
+
+    handlePasswordChange = (e) =>{
+        this.setState({
+            password: e.target.value
+        }, ()=>{
+            console.log(this.state)
+        })
+    }
+
+    handleConfirmPasswordChange = (e) =>{
+        this.setState({
+            confirm_password: e.target.value
+        }, ()=>{
+            console.log(this.state)
+        })
+    }
+
     render(){
         return(
             <React.Fragment>
-                <div class="container w-50">
-                    <h1 class="text-center my-5">S'inscrire</h1>
+                <div className="container w-50">
+                    <h1 className="text-center my-5">S'inscrire</h1>
                 <form>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Nom</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Nom</label>
+                        <input type="text"  onChange={this.handleNameChange} className="form-control" aria-describedby="emailHelp"/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Adresse email</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+                    <div className="mb-3">
+                        <label for="exampleInputEmail1" className="form-label">Adresse email</label>
+                        <input type="email" onChange={this.handleEmailChange} className="form-control" aria-describedby="emailHelp"/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Mot de passe</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Mot de passe</label>
+                        <input type="password" onChange={this.handlePasswordChange} className="form-control"/>
                     </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Confirmation mot de passe</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"/>
+                    <div className="mb-3">
+                        <label for="exampleInputPassword1" className="form-label">Confirmation mot de passe</label>
+                        <input type="password" onChange={this.handleConfirmPasswordChange} className="form-control"/>
                     </div>
-                    <button type="submit" class="btn btn-primary">S'inscrire</button>
+                    <button type="submit" className="btn btn-primary">S'inscrire</button>
                 </form>
                 </div>
             </React.Fragment>
