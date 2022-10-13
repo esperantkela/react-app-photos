00eb89003ddff0b6c294c22dda3a1694daee259a
esperantkela <esperant.kela@gmail.com>
2022-10-13 11:07:41 +0100

    react photo app

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
 
 import './App.css';
 import Welcome from './components/Welcome';
-import Car from './components/Car';
-import Login from './components/Login';
-import { BrowserRouter, Routes , Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/home">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/car">Car</Link>
-            </li>
-          </ul>
-        </nav>
-          <Routes>
-            <Route path='/home' index element={<Welcome />} />
-            <Route path="/login"  element={<Login />} />
-            <Route path="/car"  element={<Car />} />
-          </Routes>
-          
-      </div>
-
-    </BrowserRouter>
+    <Welcome/>
     </>
   );
 }
