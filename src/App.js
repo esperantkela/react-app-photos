82f18b8e92a2e3c94ada4b74dd489d6020c520f2
esperantkela <esperant.kela@gmail.com>
2022-10-13 12:39:43 +0100

    buil a login form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 
 import './App.css';
 import Home from './components/Home';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Home/>
+    <BrowserRouter>
+    <Navbar/>
+      <Routes>
+          <Route path='/' index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+class Navbar extends React.Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return (
+            <React.Fragment>
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                    <div class="container-fluid">
+                        <Link class="navbar-brand" to="/">LaraReact App</Link>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/">Accueil</Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link class="nav-link active" aria-current="page" to="/login">Connexion</Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link class="nav-link" to="/register">Inscription</Link>
+                            </li> 
+                        </ul>
+                        </div>
+                    </div>
+            </nav>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Navbar
