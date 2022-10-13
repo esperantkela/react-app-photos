bdf237745cfd107e27eb1560ee5e2e55cc48b727
esperantkela <esperant.kela@gmail.com>
2022-10-13 13:03:00 +0100

    Login component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './components/Home';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './components/Navbar';
+import Login from './components/Login';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
       <Routes>
           <Route path='/' index element={<Home />} />
+          <Route path='/login' element={<Login />} />
       </Routes>
     </BrowserRouter>
     </>
