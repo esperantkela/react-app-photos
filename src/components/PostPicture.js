1d2332ad346ff3ac6f12670134fb613dcbdbdaba
esperantkela <esperant.kela@gmail.com>
2022-10-17 12:16:35 +0100

    create picture component

---
+++ src/components/PostPicture.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class PostPicture extends React.Component{
+    constructor(){
+        super()
+        this.state = {}
+    }
+
+    render(){
+        return (
+
+        );
+    }
+}
+
+export default PostPicture
