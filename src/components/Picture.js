a586945eac36ccbe4e1817285a344525f0ce2756
esperantkela <esperant.kela@gmail.com>
2022-10-18 13:04:26 +0100

    install  sweetalert2

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,10 +1,32 @@
 import React from "react";
+import  axios  from 'axios';
 
 class Picture extends React.Component
 {
-    constructor(){
-        super()
-        this.state = {}
+    constructor(props){
+        super();
+
+        this.state = {
+           redirect : false,
+        }
+    }
+  componentDidMount() {
+     if(localStorage.getItem('token')){
+        let id = this.props.match.params.id
+        let headers = {
+            headers : {
+                'API-TOKEN' : localStorage.getItem('token')
+            }
+        }
+
+        axios.get(`http://127.0.0.1:8000/api/picture/${id}`, headers)
+            .then(res=>{
+                console.log(res)
+            })
+            .catch(error=>{
+                console.log(error.response)
+            })
+     }
     }
 
     render(){
