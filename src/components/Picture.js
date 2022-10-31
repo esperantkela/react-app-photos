396f9c1e4f9a74da9d46aea2c50769a922219e7e
esperantkela <esperant.kela@gmail.com>
2022-10-31 14:56:34 +0100

    useParams

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,43 +1,13 @@
 import React from "react";
-import  axios  from 'axios';
+import { useParams } from "react-router-dom";
 
-class Picture extends React.Component
+const  Picture  = ()=>
 {
-    constructor(){
-        super();
-
-        this.state = {
-           redirect : false,
-        }
-    }
-  componentDidMount() {
-    console.log(this.props)
-     if(localStorage.getItem('token')){
-        let id = this.props.match.params.id
-        let headers = {
-            headers : {
-                'API-TOKEN' : localStorage.getItem('token')
-            }
-        }
-
-        axios.get(`http://127.0.0.1:8000/api/picture/${id}`, headers)
-            .then(res=>{
-                this.setState({redirect:true})
-                console.log(res.response)
-            })
-            .catch(error=>{
-                console.log(error.response)
-            })
-     }else{
-        this.setState({redirect:false})
-     }
-    }
-
-    render(){
-        return (
-            <p>Photo</p>
-        );
-    }
+    const params = useParams();
+    console.log(params)
+    return(
+        <h1>Photo</h1>
+    );
 }
 
 export default Picture
