51f9d4c60af241eae7814ccc5c96dcbcc71ba35f
esperantkela <esperant.kela@gmail.com>
2022-11-02 16:16:01 +0100

    show picture

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,11 +1,48 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import  axios  from "axios";
+import React, {useState, useEffect} from "react";
+import { Navigate, useParams } from "react-router-dom";
 
 const  Picture  = ()=>
 {
+    const [redirect, setRedirect] = useState(false);
+    const [picture, setPicture] = useState({});
     const params = useParams();
-    console.log(params)
+
+    useEffect(()=>{
+        
+
+        if(localStorage.getItem('token')){
+            let id = params.id
+            let headers =  {
+                headers : {
+                    'API-TOKEN' : localStorage.getItem('token')
+                }
+            }
+
+            axios.get(`http://127.0.0.1:8000/api/pictures/${id}`, headers)
+                .then(res =>{
+                    setPicture({
+                        picture : res.data
+                    })
+                })
+                .catch(error =>{
+                    console.log(error)
+                })
+        }else{
+            setRedirect({
+                redirect : true
+            })
+        }
+      
+
+    },[] )
+
+    if(redirect){
+        return (<Navigate to='/'/>)
+    }
+
     return(
+       
         <h1>Photo</h1>
     );
 }
